c32c4eb8f51b40067d70506458195c11c3b49533
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-18 10:22:45 +0530

    Initial Commit

---
+++ plugins/callback.py
@@ -0,0 +1,185 @@
+"""
+MIT License
+
+Copyright (c) 2021 UltronRoBo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import Client, emoji
+from utils import um
+from config import Config
+playlist=Config.playlist
+
+HELP = """
+<b>Add the bot and User account in your Group with all admin rights.
+Start a VoiceChat
+Use /play <song name> or use /play as a reply to an audio file or youtube link.
+You can also use /dplay <song name> to play a song from Deezer.</b>
+**Common Commands**:
+**/play**  Reply to an audio file or YouTube link to play it or use /play <song name>.
+**/dplay** Play music from Deezer, Use /dplay <song name>
+**/player**  Show current playing song.
+**/help** Show help for commands
+**/playlist** Shows the playlist.
+**Admin Commands**:
+**/skip** [n] ...  Skip current or n where n >= 2
+**/join**  Join voice chat.
+**/leave**  Leave current voice chat
+**/vc**  Check which VC is joined.
+**/stop**  Stop playing.
+**/radio** Start Radio.
+**/stopradio** Stops Radio Stream.
+**/replay**  Play from the beginning.
+**/clean** Remove unused RAW PCM files.
+**/pause** Pause playing.
+**/resume** Resume playing.
+**/volume** Change volume(0-200).
+**/mute**  Mute in VC.
+**/unmute**  Unmute in VC.
+**/restart** Updates & restarts the Bot.
+"""
+
+
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    admins = await um.get_admins(Config.CHAT)
+    if query.from_user.id not in admins and query.data != "help":
+        await query.answer(
+            "😒 Played Joji.mp3",
+            show_alert=True
+            )
+        return
+    else:
+        await query.answer()
+    if query.data == "replay":
+        group_call = um.group_call
+        if not playlist:
+            return
+        group_call.restart_playout()
+        if not playlist:
+            pl = f"{emoji.NO_ENTRY} Empty Playlist"
+        else:
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        await query.edit_message_text(
+                f"{pl}",
+                parse_mode="Markdown",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔄", callback_data="replay"),
+                            InlineKeyboardButton("⏯", callback_data="pause"),
+                            InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                        ],
+                    ]
+                )
+            )
+
+    elif query.data == "pause":
+        if not playlist:
+            return
+        else:
+            um.group_call.pause_playout()
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Paused\n\n{pl}",
+        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔄", callback_data="replay"),
+                            InlineKeyboardButton("⏯", callback_data="resume"),
+                            InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                        ],
+                    ]
+                )
+            )
+
+    
+    elif query.data == "resume":   
+        if not playlist:
+            return
+        else:
+            um.group_call.resume_playout()
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Resumed\n\n{pl}",
+        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔄", callback_data="replay"),
+                            InlineKeyboardButton("⏯", callback_data="pause"),
+                            InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                        ],
+                    ]
+                )
+            )
+
+    elif query.data=="skip":   
+        if not playlist:
+            return
+        else:
+            await um.skip_current_playing()
+            pl = f"{emoji.PLAY_BUTTON} **Playlist**:\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+                for i, x in enumerate(playlist)
+                ])
+        try:
+            await query.edit_message_text(f"{emoji.PLAY_OR_PAUSE_BUTTON} Skipped\n\n{pl}",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔄", callback_data="replay"),
+                        InlineKeyboardButton("⏯", callback_data="pause"),
+                        InlineKeyboardButton("⏩", callback_data="skip")
+                            
+                    ],
+                ]
+            )
+        )
+        except:
+            pass
+    elif query.data=="help":
+        buttons = [
+            [
+                InlineKeyboardButton('🆘 Support/Updates Channel', url='https://t.me/UltronSupport'),
+                InlineKeyboardButton('⚙️ Support Group', url='https://t.me/UltronSupportChat'),
+            ],
+            [
+                InlineKeyboardButton('🧑‍💻 Developer', url='https://t.me/Warning_MadBoy_is_Back'),
+                InlineKeyboardButton('🗃 Source', url='https://github.com/UltronRoBo/UltronMusic/'),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.edit_message_text(
+            HELP,
+            reply_markup=reply_markup
+
+        )
