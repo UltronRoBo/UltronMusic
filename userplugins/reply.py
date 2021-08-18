0faad899b21a4d8c42c845ed218f15db54e0f049
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-18 12:50:48 +0530

    Initial Commit

---
+++ userplugins/reply.py
@@ -0,0 +1,54 @@
+"""
+MIT License
+
+Copyright (c) 2021 UltronRoBo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from pyrogram import Client, filters
+from utils import USERNAME
+from config import Config
+ADMINS=Config.ADMINS
+CACHE={}
+from pyrogram.errors import BotInlineDisabled
+@Client.on_message(filters.private & ~filters.bot & filters.incoming & ~filters.service & ~filters.me)
+async def reply(client, message): 
+    try:
+        inline = await client.get_inline_bot_results(USERNAME, "ORU_MANDAN_PM_VANNU")
+        m=await client.send_inline_bot_result(
+            message.chat.id,
+            query_id=inline.query_id,
+            result_id=inline.results[0].id,
+            hide_via=True
+            )
+        old=CACHE.get(message.chat.id)
+        if old:
+            await client.delete_messages(message.chat.id, [old["msg"], old["s"]])
+        CACHE[message.chat.id]={"msg":m.updates[1].message.id, "s":message.message_id}
+    except BotInlineDisabled:
+        for admin in ADMINS:
+            try:
+                await client.send_message(chat_id=admin, text=f"Hey,\nIt seems you have disabled Inline Mode for @{USERNAME}\n\nA Nibba is spaming me in PM, enable inline mode for @{USERNAME} from @Botfather to reply him.")
+            except Exception as e:
+                print(e)
+                pass
+    except Exception as e:
+        print(e)
+        pass
