3543aff306a4185800cdfa96055f4d418cdb8cff
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-19 09:15:08 +0530

    Initial Commit
    Test
    Sahi hua to thik, nahi to tukka

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ class Config:
     ADMIN = os.environ.get("ADMINS", '')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = int(os.environ.get("CHAT", ""))
+    CHAT =[int(os.environ.get("CHAT", "")).split()]
     LOG_GROUP=os.environ.get("LOG_GROUP", "")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
