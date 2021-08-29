62129c0bf1238aaa1f44dffd54e4234649f4d218
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-08-29 10:19:15 +0530

    Initial Commit

--- ultron.sh
+++ ultron.sh
@@ -3,7 +3,7 @@ git clone https://github.com/UltronRoBo/UltronMusic.git /UltronMusic
 cd /UltronMusic
 echo "Starting Ultron VC Player for Telegram ...."
 python3.9 music.py
-echo """                                                     +-+ +-+ +-+ +-+ +-+ +-+   +-+ +-+ +-+ +-+ +-+                                                    
-                                                             |U| |L| |T| |R| |O| |N|   |M| |U| |S| |I| |C|                                                    
-                                                             +-+ +-+ +-+ +-+ +-+ +-+   +-+ +-+ +-+ +-+ +-+                                                    
-"""
+echo "╔╦╗─╔╗─────────╔═╦═╗─╔═╦╗
+      ║║╠╗║╚╦╦╦═╦═╦╗ ║║║║╠╦╣═╬╬═╗
+      ║║║╚╣╔╣╔╣╬║║║║ ║║║║║║╠═║║═╣
+      ╚═╩═╩═╩╝╚═╩╩═╝ ╚╩═╩╩═╩═╩╩═╝"
