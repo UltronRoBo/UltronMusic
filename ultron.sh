f884932d6d6698f7b07bb0ae292b725211c9d050
Kei <73867910+KeinShin@users.noreply.github.com>
2022-01-31 16:38:16 +0530

    not needed

--- ultron.sh
+++ ultron.sh
@@ -2,11 +2,9 @@ echo "Cloning into UltronMusic ...."
 echo "+-+ +-+ +-+ +-+ +-+ +-+  +-+ +-+ +-+ +-+ +-+"
 echo "|U| |L| |T| |R| |O| |N|  |M| |U| |S| |I| |C|"
 echo "+-+ +-+ +-+ +-+ +-+ +-+  +-+ +-+ +-+ +-+ +-+"
-git clone https://github.com/UltronRoBo/UltronMusic.git /UltronMusic
-cd /UltronMusic
 echo "Starting Ultron VC Player for Telegram ...."
 echo "╔╦╗─╔╗─────────╔═╦═╗─╔═╦╗"
 echo "║║╠╗║╚╦╦╦═╦═╦╗ ║║║║╠╦╣═╬╬═╗"
 echo "║║║╚╣╔╣╔╣╬║║║║ ║║║║║║╠═║║═╣"
 echo "╚═╩═╩═╩╝╚═╩╩═╝ ╚╩═╩╩═╩═╩╩═╝"
-python3.9 music.py
+python3 music.py
